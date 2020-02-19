8e5d1ed3b35cebc7759564a48db151109e7877a6
crystalbroderick <49288075+crystalbroderick@users.noreply.github.com>
2020-02-19 14:48:45 -0800

    Add comments

--- itc240/a6/newitem_array.php
+++ itc240/a6/newitem_array.php
@@ -1,26 +1,32 @@
 <?php
+//Insert a new item in an array with elements 1, 2, 3, 4, 5, 6
+//Result: The array should now contain 1,2,3,4, 42, 5, 6
+
+
 //Original Array on which operations is to be perform
 $numbers = array( '1', '2', '3', '4', '5', '6' );
   
 //value of new item
 $inserted_value = '42';
   
-//value of position at which insertion is to be done
-  
+//value of position at which insertion is to be done 
 $position = 4;
   
-//array_splice() function 
-  
-array_splice( $numbers, $position, 0, $inserted_value ); 
+//array_splice() function
+//adds inserted value into array position  
+array_splice( $numbers, $position, 0, $inserted_value );
+
+//count for stopping comma
 $num_of_items = count($numbers);
 $num_count=0;
+
 
 echo "The array should now contain ";
 foreach ($numbers as $number){
     echo $number;
     $num_count = $num_count + 1;
     
-    if ($num_count < $num_of_items) {
+    if ($num_count < $num_of_items) {   //stops comma before last count in array
         echo ", ";
     }
 }
--- itc240/a6/pets_array.php
+++ itc240/a6/pets_array.php
@@ -1,4 +1,12 @@
 <?php
+
+//Given the following of pets containing the items ('dog', 'cat', 'fish', 'bird', 'iguana');
+//display the following text, pulling the animal types from the pets array. Result: "You can adopt a dog, a cat, or a bird from the animal sanctuary."
+
+//list of pets
 $pets = array('dog', 'cat', 'fish', 'bird', 'iguana');
-echo "You can adopt a $pets[0], a  $pets[1], or a $pets[3] from the animal sanctuary.";   
+
+//pulls items from array
+echo "You can adopt a $pets[0], a  $pets[1], or a $pets[3] from the animal sanctuary.";
+
 ?>
